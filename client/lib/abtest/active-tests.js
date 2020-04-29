f1aa7be6263b084e573162cf601190a90e0c533e
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2020-04-29 19:33:49 +0530

    White glove MVP

--- client/lib/abtest/active-tests.js
+++ client/lib/abtest/active-tests.js
@@ -136,4 +136,13 @@ export default {
 		allowExistingUsers: true,
 		localeTargets: 'any',
 	},
+	whiteGloveUpsell: {
+		datestamp: '20200428',
+		variations: {
+			variantShowOffer: 50,
+			control: 50,
+		},
+		defaultVariation: 'control',
+		allowExistingUsers: false,
+	},
 };
